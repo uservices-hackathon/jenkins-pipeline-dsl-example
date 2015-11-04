f3081df841fbbb64a740d4431596dca257ca298b
Lukasz Szczesny <luk@wybcz.pl>
2015-11-04 15:47:48 +0100

    Add some test job

---
+++ jobs/test_job.groovy
@@ -0,0 +1,13 @@
+
+def giturl = 'https://github.com/quidryan/aws-sdk-test.git'
+
+for(i in 0..10) {
+    job("DSL-Tutorial-1-Test-${i}") {
+        scm {
+            git(giturl)
+        }
+        steps {
+            maven("test -Dtest.suite=${i}")
+        }
+    }
+}
